ea2f7730127e4dc93b88d86c6c13fc5c942b3839
Akash-Hajare <akashhajare97@gmail.com>
2023-01-14 20:02:50 +0530

    check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,9 @@ package com.driver;
 
 public class Main {
    public static void main(String[]args){
-       B b= new B();
-       String ans=b.meth();
-       System.out.println(ans);
+//       B b= new B();
+//       String ans=b.meth();
+//       System.out.println(ans);
    }
 }
 
