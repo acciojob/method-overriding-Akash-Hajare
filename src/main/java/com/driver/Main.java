ba2e7b9ba3208a0b8c5f7596becbbee3a33d9fae
Akash-Hajare <akashhajare97@gmail.com>
2023-01-16 12:23:32 +0530

    makes class Static

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,18 @@ package com.driver;
 
 public class Main {
    public static void main(String[]args){
-//       B b= new B();
-//       String ans=b.meth();
-//       System.out.println(ans);
+       B b= new B();
+       b.meth();
+   }
+   static public class A {
+      public String meth(){
+           return  "Invoking method from class A";
+       }
+   }
+   static public class B extends A {
+       public String meth() {
+           return "Method is overridden in Extendend class B";
+       }
    }
 }
 
